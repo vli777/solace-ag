e3f5134ce5dff2d0236b44fa653fef3b607addc0
vintg <10236284+vintg@users.noreply.github.com>
2024-12-16 17:17:38 -0500

    feat: POST route for /advocates

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,31 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { NextResponse } from "next/server";
+import db from "@/db";
+import { advocates } from "@/db/schema";
+import buildQuery from "@/app/utils/buildQuery";
+import { count } from "drizzle-orm";
+import { buildAdvocatesQuery } from "./buildAdvocatesQuery";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+    const { filterModel, sortModel, limit, offset } = body
 
-  const data = advocateData;
+    const query = buildAdvocatesQuery(filterModel, sortModel, { limit, offset })    
+    const data = await query
+    
+    // get total count of rows for pagination usage
+    // @ts-ignore
+    const totalQuery = db.select({ total: count() }).from(advocates);
+    const [{ total }] = await buildQuery(totalQuery, advocates, filterModel, [])
 
-  return Response.json({ data });
+    return NextResponse.json({
+      data,
+      total
+    })
+  } catch (error) {
+    const errorMessage = "Error fetching /api/advocates"
+    console.error(errorMessage, error);
+
+    return NextResponse.json({ status: 500, error: errorMessage })
+  }
 }
