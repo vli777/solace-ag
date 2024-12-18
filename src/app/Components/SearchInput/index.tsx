da42063c5d1c1085d30aee156562071f4a78b6e0
vintg <10236284+vintg@users.noreply.github.com>
2024-12-17 23:20:44 -0500

    feat: pressing enter should also search

--- src/app/Components/SearchInput/index.tsx
+++ src/app/Components/SearchInput/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { KeyboardEvent } from "react";
 
 type SearchInputProps = {
   searchTerm: string;
@@ -9,25 +9,34 @@ type SearchInputProps = {
 
 
 export const SearchInput = ({ searchTerm, onSearch, onClear, onChange }: SearchInputProps) => {  
+  const onKeyDownSearch = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSearch();
+    }
+  };
+
   return (
-    <div className="relative w-full">
-      <input
-        className="border rounded p-2 w-full pr-10"
-        placeholder="Search"
-        value={searchTerm}
-        onChange={onChange}
-      />
-      {searchTerm && (
-        <button
-          onClick={onClear}
-          className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-transparent text-gray-500 hover:text-gray-800"
-        >
-          &times;
-        </button>
-      )}
+    <div className="flex items-center gap-2 w-full">
+      <div className="relative w-full">
+        <input
+          className="border rounded p-2 w-full pr-10"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={onChange}
+          onKeyDown={onKeyDownSearch}
+        />
+        {searchTerm && (
+          <button
+            onClick={onClear}
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-transparent text-gray-500 hover:text-gray-800"
+          >
+            X
+          </button>
+        )}
+      </div>
       <button
         onClick={onSearch}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 ml-2"
+        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
       >
         Search
       </button>
